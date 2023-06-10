257ff8c7dd5f3fd132daecac158f806c144adb7f
Dypso <dypsok@gmail.com>
2023-06-11 00:47:01 +0200

    Etape2

--- Controllers/ProduitsController.cs
+++ Controllers/ProduitsController.cs
@@ -16,3 +16,26 @@
 
             return produit;
         }
+
+        [HttpPost]
+        public async Task<ActionResult<Produit>> CreateProduit(Produit produit)
+        {
+            await _produitsService.CreateProduit(produit);
+
+            return CreatedAtAction(nameof(GetProduit), new { id = produit.Id }, produit);
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> UpdateProduit(string id, Produit produitIn)
+        {
+            var produit = await _produitsService.GetProduit(id);
+
+            if (produit == null)
+            {
+                return NotFound();
+            }
+
+            await _produitsService.UpdateProduit(id, produitIn);
+
+            return NoContent();
+        }
